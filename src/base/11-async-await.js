0fb58fe65ab8897b7a88b6963bce580a7b88a694
Luis Miguel Rodriguez <36597427+luismigueldev1@users.noreply.github.com>
2021-04-28 11:57:04 -0400

    Update 11-async-await.js

--- src/base/11-async-await.js
+++ src/base/11-async-await.js
@@ -1,6 +1,6 @@
 export const getImage = async () => {
   try {
-    const API_KEY = "Au2dPNpfu08JAXnQygyU7oKe7cEx1uP5";
+    const API_KEY = "GIPHY API KEY HERE";
     const URL = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
     const response = await fetch(URL);
     const {
